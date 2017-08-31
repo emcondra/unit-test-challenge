f37030fa8d41b5bbbbbcbc293c0f3374864ebcab
econdra <emcondra@gmail.com>
2017-08-31 11:53:54 -0500

    Change name of test

--- UnitTestChallenge/CandyCreator_should_.cs
+++ UnitTestChallenge/CandyCreator_should_.cs
@@ -6,10 +6,10 @@ namespace UnitTestChallenge
     public class CandyCreatorShould
     {
         [Test]
-        public void create_candy_with_dark_chocolate()
+        public void create_candy_with_dark_chocolate_and_10000_calories()
         {
             var candyStub = new Stub<Candy>();
-            candyStub.Setup(c => c.Calories).Returns(100);
+            candyStub.Setup(c => c.Calories).Returns(10000);
 
             var candyCreatorStub = new Stub<CandyCreator>();
 
